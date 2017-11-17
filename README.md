d00aba3b659e0eeb444e18ff3d48b8f247634384
Dingo <oferdottan@gmail.com>
2017-11-17 05:59:03 +0200

    Revert "Revert "A18 Version (early commit)""
    This reverts commit ad5c2befce56a530848545dd76c9d43e8c368d34, reversing
    changes made to e5a5f33c3d71e385e9da468bf6f1d5a946160c38.

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-![Notifications Archiver](http://i.imgur.com/aoPyKFi.png)
+![Notifications Archiver](https://i.imgur.com/LiOXVXW.png)
 
-Adds a tab at the bottom of the screen titled "Archive".
+This mod introduces the "Archive" tab.
 
-Allows you to browse and re-open old letters and messages.
+The Archive preserves pop-up notifications and lets you browse them on-demand. This includes the temporary messages at the top of the in-game UI.
+
+You can browse by notification type, filter by keywords and pin important archives. Pinning will prevent an archive from being deleted.
+
+Left-click to open a letter or jump to a message's target (if one exists).
+
+Right-click to pin or delete archives, in-case the list gets a bit crowded.
 
 Powered by [Harmony](https://github.com/pardeike/Harmony)
+
+Pin icon by Freepik (https://www.freepik.com/)
---
+++ Source/Archive/MasterArchive.cs
@@ -0,0 +1,142 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+
+namespace Notifications_Archiver
+{
+	public class MasterArchive : IExposable
+	{
+		public ArchiveType type;
+
+		public bool pinned;
+
+		public int dateDayofSeason = -1;
+
+		public Quadrum dateQuadrum = Quadrum.Undefined;
+
+		public int dateYear = -1;
+
+		public Letter letter;
+
+		public GlobalTargetInfo lookTarget;
+
+		private string text;
+
+		private void SetDateInfo()
+		{
+			//RimWorld.DateReadout.DateOnGUI
+			Vector2 location;
+
+			if (WorldRendererUtility.WorldRenderedNow && Find.WorldSelector.selectedTile >= 0)
+			{
+				location = Find.WorldGrid.LongLatOf(Find.WorldSelector.selectedTile);
+			}
+
+			else if (WorldRendererUtility.WorldRenderedNow && Find.WorldSelector.NumSelectedObjects > 0)
+			{
+				location = Find.WorldGrid.LongLatOf(Find.WorldSelector.FirstSelectedObject.Tile);
+			}
+
+			else
+			{
+				if (Find.VisibleMap == null)
+				{
+					return;
+				}
+
+				location = Find.WorldGrid.LongLatOf(Find.VisibleMap.Tile);
+			}
+
+			int gameTicks = Find.TickManager.gameStartAbsTick == 0 ? Find.TickManager.TicksGame : Find.TickManager.TicksAbs; //Find.TickManager.TicksAbs errors if gameStartAbsTick is 0
+
+			//RimWorld.GenDate.DateReadoutStringAt
+			this.dateDayofSeason = GenDate.DayOfSeason(gameTicks, location.x) + 1;
+			this.dateQuadrum = GenDate.Quadrum(gameTicks, location.x);
+			this.dateYear = GenDate.Year(gameTicks, location.x);
+		}
+
+		public string Label
+		{
+			get
+			{
+				if (this.letter != null)
+				{
+					return this.letter.label;
+				}
+
+				return this.text;
+			}
+		}
+
+		public string Text
+		{
+			get
+			{
+				if (this.letter is ChoiceLetter)
+				{
+					ChoiceLetter choiceLet = this.letter as ChoiceLetter;
+
+					return choiceLet.text;
+				}
+
+				return this.text;
+			}
+		}
+
+		public void ClickAction()
+		{
+			if (this.letter != null)
+			{
+				this.letter.OpenLetter();
+			}
+
+			if (this.lookTarget != GlobalTargetInfo.Invalid)
+			{
+				CameraJumper.TryJumpAndSelect(this.lookTarget);
+			}
+		}
+
+		public MasterArchive()
+		{
+			this.SetDateInfo();
+		}
+
+		//Letter constructor
+		public MasterArchive(Letter let) : this()
+		{
+			this.type = ArchiveType.Letter;
+			this.letter = let;
+		}
+
+		//Message constructor
+		public MasterArchive(string content, GlobalTargetInfo target) : this()
+		{
+			this.type = ArchiveType.Message;
+			this.text = content;
+			this.lookTarget = target;
+		}
+
+		public void ExposeData()
+		{
+			if (Scribe.mode == LoadSaveMode.Saving)
+			{
+				if (!this.lookTarget.IsValid || this.lookTarget.ThingDestroyed || (this.lookTarget.HasThing && this.lookTarget.Thing.MapHeld == null))
+				{
+					this.lookTarget = GlobalTargetInfo.Invalid;
+				}
+			}
+
+			Scribe_Values.Look(ref this.type, "type");
+			Scribe_Values.Look(ref this.pinned, "pinned", false);
+
+			Scribe_Values.Look(ref this.dateDayofSeason, "dateDayofSeason", -1);
+			Scribe_Values.Look(ref this.dateQuadrum, "dateQuadrum", Quadrum.Undefined);
+			Scribe_Values.Look(ref this.dateYear, "dateYear", -1);
+
+			Scribe_Deep.Look(ref this.letter, "letter", new object[0]);
+			Scribe_TargetInfo.Look(ref this.lookTarget, "lookTarget");
+			Scribe_Values.Look(ref this.text, "text");
+		}
+	}
+}
